84b467b753fd16579d0e0d64a9e0ab4499503208
Alkaponees <80576881+Alkaponees@users.noreply.github.com>
2024-11-20 16:18:32 +0200

    Update check_docker_compose.sh

--- check_docker_compose.sh
+++ check_docker_compose.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if docker compose version &> /dev/null; then
-  echo "Docker Compose is installed..."
+  echo "Docker Compose is installed...";
 else
-  echo "Docker Compose is not install. Starting installation..."
-  curl -SL https://github.com/docker/compose/releases/download/v2.30.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+  echo "Docker Compose is not install. Starting installation...";
+  curl -SL https://github.com/docker/compose/releases/download/v2.30.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose;
+  chmod +x /usr/local/bin/docker-compose;
+fi
